ddc958d8d7b7a937c958185e8af0396ed31af563
Allan <allan.xiao@gmail.com>
2016-12-10 16:53:36 -0500

    Fix typo

--- pages/Portfolio.js
+++ pages/Portfolio.js
@@ -234,7 +234,7 @@ $("document").ready(function () {
             {
                 "img": "/img/personal/utils.png",
                 "onClick": "showUtils()",
-                "name": "innouvoua Utila"
+                "name": "innouvous Utila"
             },
             {
                 "img": "/img/personal/sudoku.png",
